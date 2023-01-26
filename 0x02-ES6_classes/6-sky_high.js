9f6b609539625b265c76b647a54e4637074a5483
“victornnamdii” <victorilodiuba@gmail.com>
2023-01-26 21:44:25 +0100

    Add: 0x02/6

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-underscore-dangle */
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this.floors = floors;
+  }
+
+  evacuationWarningMessage() {
+    return (`Evacuate slowly the ${this.floors} floors`);
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(value) {
+    if (typeof value !== 'number') {
+      throw new Error('Floors must be a number');
+    }
+    this._floors = value;
+  }
+}
