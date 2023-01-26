101b62e3634fde9933245c9b8445bd1c6ed5b448
“victornnamdii” <victorilodiuba@gmail.com>
2023-01-26 20:23:01 +0100

    ESLint fix

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,5 +1,6 @@
 export default class ClassRoom {
-  constructor (maxStudentsSize) {
-    this._maxStudentsSize = maxStudentsSize
+  constructor(maxStudentsSize) {
+    // eslint-disable-next-line no-underscore-dangle
+    this._maxStudentsSize = maxStudentsSize;
   }
 }
