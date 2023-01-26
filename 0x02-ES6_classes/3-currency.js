bdfaf318c3f7787c627650b9ffe07d687b99610e
“victornnamdii” <victorilodiuba@gmail.com>
2023-01-26 21:07:11 +0100

    Add: 0x02/3

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,33 @@
+/* eslint-disable no-underscore-dangle */
+export default class Currency {
+  constructor(code, name) {
+    this.code = code;
+    this.name = name;
+  }
+
+  displayFullCurrency() {
+    return (`${this.name} (${this.code})`);
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    if (typeof value !== 'string') {
+      throw new Error('Code must be a string');
+    }
+    this._code = value;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new Error('Name must be a string');
+    }
+    this._name = value;
+  }
+}
