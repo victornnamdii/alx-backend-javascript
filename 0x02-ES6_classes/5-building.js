a0f5bca006a6b7323178fdb1866a047cd2062d15
“victornnamdii” <victorilodiuba@gmail.com>
2023-01-26 21:37:51 +0100

    Add: 0x02/5

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-underscore-dangle */
+export default class Building {
+  constructor(sqft) {
+    this.sqft = sqft;
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error('Class extending Building must override evacuationWarningMessage');
+      }
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(value) {
+    if (typeof value !== 'number') {
+      throw new Error('Square feet must be a number');
+    }
+    this._sqft = value;
+  }
+}
