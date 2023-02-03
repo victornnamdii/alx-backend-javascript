b6f40f6edb7ab7ac522fa1d3851fa51ce998d154
“victornnamdii” <victorilodiuba@gmail.com>
2023-02-03 08:57:30 +0100

    Add task_2

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,62 @@
+interface DirectorInterface {
+    workFromHome(): string,
+    getCoffeeBreak(): string,
+    workDirectorTasks(): string
+}
+
+interface TeacherInterface {
+    workFromHome(): string,
+    getCoffeeBreak(): string,
+    workTeacherTasks(): string
+}
+
+class Director implements DirectorInterface {
+    workFromHome(): string {
+        return 'Working from home';
+    }
+    getCoffeeBreak(): string {
+        return 'Getting a coffee break';
+    }
+    workDirectorTasks(): string {
+        return 'Getting to director tasks';
+    }
+}
+
+class Teacher implements TeacherInterface {
+    workFromHome(): string {
+        return 'Cannot work from home';
+    }
+    getCoffeeBreak(): string {
+        return 'Cannot have a break';
+    }
+    workTeacherTasks(): string {
+        return 'Getting to work';
+    }
+}
+
+function createEmployee(salary: (number | string)): (Director | Teacher) {
+    if (typeof salary === 'number' && salary < 500) {
+        return new Teacher();
+    }
+    return new Director();
+}
+
+function isDirector(employee: (Director | Teacher)): boolean {
+    return employee instanceof Director;
+}
+
+function executeWork(employee: (Director | Teacher)): string {
+    if (isDirector(employee)) {
+        return (employee as Director).workDirectorTasks();
+    }
+    return (employee as Teacher).workTeacherTasks();
+}
+
+type Subjects = ('Math' | 'History');
+
+function teachClass(todayClass: Subjects) {
+    if (todayClass === 'Math') {
+        return 'Teaching Math';
+    }
+    return 'Teaching History';
+}
