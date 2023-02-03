fd2759161e74d8b6975d703ed211e70ec875e881
“victornnamdii” <victorilodiuba@gmail.com>
2023-02-03 21:06:01 +0100

    task_4

--- 0x04-TypeScript/task_3/js/main.ts
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -3,8 +3,8 @@ import { RowID, RowElement } from './interface';
 import * as CRUD from './crud';
 
 const row: RowElement = {
-  firstName: Guillaume,
-  lastName: Salva,
+  firstName: 'Guillaume',
+  lastName: 'Salva',
 }
 
 const newRowID: RowID = CRUD.insertRow(row);
